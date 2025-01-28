50f2733731a7857c44d66f4212e0f05a20707da4
Elouan RIGAUT <elouan.rigaut8@etu.univ-lorraine.fr>
2025-01-28 15:37:15 +0100

    added main and arg pass

--- ripdroid.py
+++ ripdroid.py
@@ -1,3 +1,7 @@
 import os
+import argparse
 
-os.system("adb shell \"dumpsys wifi\" > ./data/dumpsys.txt")
+
+if __name__ == "__main__":
+    if(os.system("adb shell \"dumpsys wifi\" > ./data/dumpsys.txt") != 0):
+        print("Please connect a phone via USB and make sure it is detected by ADB.")
