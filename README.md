aaa84eafec821c78af869ee34a85ee5ff01b310a
ElouanRigaut <elouan.rigaut8@etu.univ-lorraine.fr>
2025-01-29 18:03:07 +0100

    added parser logic

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ RipDroid, a Forensic Android Data Extractor (wifi history extraction)
 
 This is a python script, made for threat intelligence purpose, to analyse data from an android phone with ADB enabled.
 
+
+
 ## How to run
 
 1. Install Android Platform Tools and install to path   
--- ripdroid.py
+++ ripdroid.py
@@ -3,5 +3,38 @@ import argparse
 
 
 if __name__ == "__main__":
-    if(os.system("adb shell \"dumpsys wifi\" > ./data/dumpsys.txt") != 0):
-        print("Please connect a phone via USB and make sure it is detected by ADB.")
+
+    # Parser Logic
+    parser = argparse.ArgumentParser(
+        prog="RipDroid",
+        description="An android forensic tool"
+    )
+    parser.add_argument('-a', '--all', action='store_true', help="Print the accounts, connectivity logs and WiFi SSID stored on the device")
+    parser.add_argument('--account', action='store_true', help="Print the accounts stored on the device")
+    parser.add_argument('--connectivity', action='store_true', help="Print the connectivity log (Wifi/Cellular) the device")
+    parser.add_argument('--wifi', action='store_true', help="Print the stored WiFi SSID on the device")
+    args = parser.parse_args()
+
+
+    # Treating args
+    if not any(vars(args).values()):
+        print("No arguments provided.")
+        exit(1)
+    
+    all = False
+    if args.all:
+        all = True
+
+    if all or args.account:
+        if(os.system("adb shell \"dumpsys account\" > ./data/account.txt") != 0):
+            print("Please connect a phone via USB and make sure it is detected by ADB.")
+    
+    if all or args.connectivity:
+        if(os.system("adb shell \"dumpsys connectivity\" > ./data/connectivity.txt") != 0):
+            print("Please connect a phone via USB and make sure it is detected by ADB.")
+    
+    if all or argparse.Namespace(wifi=True):
+        if(os.system("adb shell \"dumpsys wifi\" > ./data/wifi.txt") != 0):
+            print("Please connect a phone via USB and make sure it is detected by ADB.")
+    
+    
